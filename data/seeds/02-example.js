ac01f97947797ce0bdd0bce43acbdc3ae7d4f329
Michael Nunes <miketnunes@gmail.com>
2019-12-18 10:01:24 -0800

    Generate database migration, seed

--- data/seeds/02-example.js
+++ data/seeds/02-example.js
@@ -1,13 +1,11 @@
+const bcrypt = require("bcrypt");
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("users").insert([
+    { username: "Darrin", password: bcrypt.hashSync("password", 8), department: "Software" },
+    { username: "Michael", password: bcrypt.hashSync("password", 8), department: "Software" },
+    { username: "Joah", password: bcrypt.hashSync("password", 8), department: "Accounting" },
+    { username: "Oliver", password: bcrypt.hashSync("password", 8), department: "Accounting" },
+    { username: "Pedro", password: bcrypt.hashSync("password", 8), department: "Finance" },
+  ]);
 };
